59e9a43726b085f43ef20f11633f6733f2864e63
khc_9 <khc_961021@naver.com>
2022-07-15 13:09:37 +0900

    test1

---
+++ cake/src/test/java/com/its/cake/CakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its.cake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
